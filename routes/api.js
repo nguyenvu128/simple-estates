9855c3b6f5d9bf318e3ad364633de40919fa09d4
NGUYENVU <nguyenvu.hcmus@gmail.com>
2020-06-05 16:05:22 +0700

    added router.use of auth user

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router({});
 const postsRouter = require('./posts.route');
 const statisticRouter = require('./statistic.route');
+const authUserRouter = require('./auth-users.route');
 
 
 router.use('/posts', postsRouter);
 router.use('/statistic', statisticRouter);
+router.use('/auth', authUserRouter);
 
 module.exports = router;
