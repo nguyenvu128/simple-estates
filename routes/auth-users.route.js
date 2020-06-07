5e71de3f486cc7a9096dca22352c70d1fb465f73
NGUYENVU <nguyenvu.hcmus@gmail.com>
2020-06-07 12:13:00 +0700

    added path of api forget

--- routes/auth-users.route.js
+++ routes/auth-users.route.js
@@ -5,5 +5,6 @@ const usersController = require('../controllers/users.controller');
 router.post('/login', usersController.userLogin);
 router.post('/register', usersController.registerNewUser);
 router.post('/confirm', usersController.confirmUser);
+router.post('/forget', usersController.forgetPassword);
 
 module.exports = router;
