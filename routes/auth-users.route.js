0ff6dfc64ed5715df654d664d5921c173eda976e
NGUYENVU <nguyenvu.hcmus@gmail.com>
2020-06-08 00:47:59 +0700

    added path api reset

--- routes/auth-users.route.js
+++ routes/auth-users.route.js
@@ -6,5 +6,6 @@ router.post('/login', usersController.userLogin);
 router.post('/register', usersController.registerNewUser);
 router.post('/confirm', usersController.confirmUser);
 router.post('/forget', usersController.forgetPassword);
+router.post('/reset', usersController.resetPassword);
 
 module.exports = router;
