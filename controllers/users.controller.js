d39768837d85c4d13437df1f80d61b4dead5ae08
NGUYENVU <nguyenvu.hcmus@gmail.com>
2020-06-08 12:29:07 +0700

    added confirmPassword

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -209,7 +209,7 @@ const forgetPassword = async (req, res) => {
 };
 
 const resetPassword = async (req, res) => {
-    const {token, password} = req.body;
+    const {token, password, confirmPassword} = req.body;
 
     const checkedPassword = isValidatorPassword(req.body, res);
     if(checkedPassword === false){
