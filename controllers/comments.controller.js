478974d6f41391c839960e554f0a410b525a4df4
NGUYENVU <nguyenvu.hcmus@gmail.com>
2020-06-13 19:59:58 +0700

    create comments.controller file and handle function create a comment

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -1,18 +1,46 @@
 const mongoose = require('mongoose');
 const HttpStatus = require('http-status-codes');
-const PostModel = require('../models/post.model');
-const UsersModel = require('../models/users.model');
 const CommentsModel = require('../models/comments.model');
 
 const createComment = async (req, res) => {
     try {
         const {type, postId, commentId, content} = req.body;
+        if(type !== 'POST' || type !== 'COMMENT') {
+            return res.status(HttpStatus.BAD_REQUEST).json({
+                message: "Không hợp lệ"
+            });
+        }
+
         const query = {};
         if(type === 'POST') {
+            if(!postId) {
+                return res.status(HttpStatus.BAD_REQUEST).json({
+                    message: "Không hợp lệ"
+                });
+            }
+
+            if(postId.length !== 24) {
+                return res.status(HttpStatus.BAD_REQUEST).json({
+                    message: "Không hợp lệ"
+                });
+            }
+
             query.postId = postId;
         }
 
         if(type === "COMMENT") {
+            if(!commentId) {
+                return res.status(HttpStatus.BAD_REQUEST).json({
+                    message: "Không hợp lệ"
+                });
+            }
+
+            if(commentId.length !== 24) {
+                return res.status(HttpStatus.BAD_REQUEST).json({
+                    message: "Không hợp lệ"
+                });
+            }
+
             query.commentId = commentId;
         }
 
@@ -30,9 +58,17 @@ const createComment = async (req, res) => {
             });
         }
 
-        // const commentDb = new CommentsModel ({
-        //     userId:
-        // });
+        const commentDb = new CommentsModel ({
+            userId: req.user._id,
+            postId: postId,
+            text: content,
+            parentCommentId: commentId
+        });
+
+        await commentDb.save();
+        return res.status(HttpStatus.OK).json({
+            message: "Thành công"
+        });
 
     } catch (e) {
         console.log(e);
