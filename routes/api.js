7b3829298fbbcd1d6f2bdd32890fc157db4e2918
NGUYENVU <nguyenvu.hcmus@gmail.com>
2020-06-13 20:19:28 +0700

    fixed path of commentsRouter

--- routes/api.js
+++ routes/api.js
@@ -3,10 +3,11 @@ const router = express.Router({});
 const postsRouter = require('./posts.route');
 const statisticRouter = require('./statistic.route');
 const authUserRouter = require('./auth-users.route');
-
+const commentsRouter = require('./comments.route');
 
 router.use('/posts', postsRouter);
 router.use('/statistic', statisticRouter);
 router.use('/auth', authUserRouter);
+router.use('/comments', commentsRouter);
 
 module.exports = router;
