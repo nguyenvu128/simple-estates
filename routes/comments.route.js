a27b39ccfd50e761121e3569b7e796c546caa507
NGUYENVU <nguyenvu.hcmus@gmail.com>
2020-06-17 14:37:17 +0700

    add router api get list comments

--- routes/comments.route.js
+++ routes/comments.route.js
@@ -4,5 +4,6 @@ const CommentsController = require('../controllers/comments.controller');
 const checkToken = require('../middlewares/check-token.middleware');
 
 router.post('/', checkToken, CommentsController.createComment);
+router.get('/', checkToken, CommentsController.getListComments);
 
 module.exports = router;
